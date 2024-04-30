75889e08635f6f697cddefd5687a9e36c8c6a214
ClaudioPSQ <168118913+ClaudioPSQ@users.noreply.github.com>
2024-04-30 14:19:35 +0200

    Crea e mascola mazzo di carte

--- scriptbriscola.js
+++ scriptbriscola.js
@@ -34,3 +34,54 @@ function closeModal(modal) {
   modal.classList.remove('active')
   overlay.classList.remove('active')
 }
+
+
+
+
+
+const SEMI = {
+  COPPE: 'Coppe',
+  SPADE: 'Spade',
+  DENARI: 'Denari',
+  BASTONI: 'Bastoni'
+};
+
+const VALORI = {
+  ASSO: 'Asso',
+  DUE: 'Due',
+  TRE: 'Tre',
+  QUATTRO: 'Quattro',
+  CINQUE: 'Cinque',
+  SEI: 'Sei',
+  SETTE: 'Sette',
+  FANTE: 'Fante',
+  CAVALLO: 'Cavallo',
+  RE: 'Re'
+};
+
+function creaMazzo() {
+  let mazzo = [];
+  for (let seme in SEMI) {
+    for (let valore in VALORI) {
+      mazzo.push({ seme: SEMI[seme], valore: VALORI[valore] });
+    }
+  }
+  return mazzo;
+}
+
+function mescolaMazzo(mazzo) {
+  for (let i = mazzo.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [mazzo[i], mazzo[j]] = [mazzo[j], mazzo[i]];
+  }
+  return mazzo;
+}
+
+let mazzoDiCarte = [];
+
+window.onload = function() {
+  mazzoDiCarte = creaMazzo();
+  mazzoDiCarte = mescolaMazzo(mazzoDiCarte);
+  console.log(mazzoDiCarte); // Stampa il mazzo di carte mescolato nella console
+}
+
