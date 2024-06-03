2e2579f9c070e3a3a10098357c4038a8eb146935
chiasrfn <127093935+chiasrfn@users.noreply.github.com>
2024-06-03 20:47:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php 
 session_start();
 
-if (isset($_COOKIE['remember']) && !isset($_SESSION['nome_utente'])){
-  header("Location: biscotto.php");
-  exit();
-}
-
-
-
 if (!isset($_SESSION['nome_utente'])) {
 
  ?>
-
 
 
 <!DOCTYPE html>
@@ -65,7 +57,7 @@ if (!isset($_SESSION['nome_utente'])) {
     </div>
     <div class="modal-remember">
       <label>
-        <input type="checkbox" name="remember" id="remember">Ricordami</input>
+        <input type="checkbox" id="remember">Ricordami</input>
       </label>
       <button data-modal-target=#forgotmodal class="button login">Hai dimenticato la password?</button>
     </div>
@@ -80,19 +72,19 @@ if (!isset($_SESSION['nome_utente'])) {
   </form>
 
   <!--login forgot-->
-  <form method="post" action="password-temp.php" id="forgotmodal" class="modal login forgot">
+  <div id="forgotmodal" class="modal login forgot">
     <div class="modal-header">
       <h1 id="titlePassword">Password</br>dimenticata?</h1>
       <button data-close-button class="close-button" id="close-button-login">&times;</button>
     </div>
     <p style="text-align: center;">Inserisci la tua email, dopo</br> averla inviata controlla <br> la tua casella postale</p>
     <div class="modal-input" id="myForm">
-      <input type="text" name="email_pswdimenticata" id="email" placeholder="Email" required>
+      <input type="text" id="email" placeholder="Email" required>
       <i class="bi bi-envelope-fill"></i>
-      <span id="emailError" style="color: red; display: none;">Perfavore inserisci una email valida</span>
+      <span id="emailError" style="color: red; display: none;">Perfavore metti una email valida</span>
     </div>
-    <button type="submit" class="modal-btn inviaemail" id="inviaemail">Invia</button>
-</form>
+    <button class="modal-btn inviaemail" id="inviaemail">Invia</button>
+  </div>
 
 
   <!--login registrati-->
